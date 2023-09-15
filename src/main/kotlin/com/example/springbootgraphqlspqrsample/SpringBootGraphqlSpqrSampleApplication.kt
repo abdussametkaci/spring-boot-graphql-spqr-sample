361125c3925cd417c8222471acb88e963a581f77
Abdussamet KACI <abdussamet.kaci@d-teknoloji.com.tr>
2023-09-15 11:34:44 +0300

    Initial Commit

---
+++ src/main/kotlin/com/example/springbootgraphqlspqrsample/SpringBootGraphqlSpqrSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springbootgraphqlspqrsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootGraphqlSpqrSampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootGraphqlSpqrSampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springbootgraphqlspqrsample/SpringBootGraphqlSpqrSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springbootgraphqlspqrsample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootGraphqlSpqrSampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
