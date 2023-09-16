6107e4612bfb50ed061dfb6e09b0b3ae2d1eccad
Abdussamet KACI <abdussamet.kaci@d-teknoloji.com.tr>
2023-09-16 13:17:39 +0300

    Override DefaultTypeInfoGenerator#generateInputTypeName for remove default 'Input' suffix

---
+++ src/main/kotlin/com/example/springbootgraphqlspqrsample/configuration/InputGenerator.kt
@@ -0,0 +1,21 @@
+package com.example.springbootgraphqlspqrsample.configuration
+
+import io.leangen.graphql.metadata.messages.MessageBundle
+import io.leangen.graphql.metadata.strategy.type.DefaultTypeInfoGenerator
+import org.springframework.context.annotation.Configuration
+import java.lang.reflect.AnnotatedType
+
+@Configuration
+class InputGenerator : DefaultTypeInfoGenerator() {
+
+    override fun generateInputTypeName(type: AnnotatedType?, messageBundle: MessageBundle?): String {
+        super.generateInputTypeName(type, messageBundle)
+        return this.generateTypeName(type, messageBundle)
+    }
+}
+
+/*
+The DefaultTypeInfoGenerator#generateInputTypeName method has been overridden to prevent the input suffix from being added.
+Original Code: return generateTypeName(type, messageBundle) + INPUT_SUFFIX;
+Also, SCALAR_SUFFIX is added for scalar. If you don't want this to happen, you need to override DefaultTypeInfoGenerator#generateScalarTypeName.
+*/
