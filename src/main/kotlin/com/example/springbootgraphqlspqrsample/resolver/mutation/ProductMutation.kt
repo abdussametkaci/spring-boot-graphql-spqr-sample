d94f7e8cb605654f3f5e1fe0239edd07e2a8f200
Abdussamet KACI <abdussamet.kaci@d-teknoloji.com.tr>
2023-09-16 23:28:11 +0300

    Add update and delete mutation

--- src/main/kotlin/com/example/springbootgraphqlspqrsample/resolver/mutation/ProductMutation.kt
+++ src/main/kotlin/com/example/springbootgraphqlspqrsample/resolver/mutation/ProductMutation.kt
@@ -7,6 +7,7 @@ import io.leangen.graphql.annotations.GraphQLMutation
 import io.leangen.graphql.annotations.GraphQLNonNull
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi
 import org.springframework.stereotype.Component
+import java.util.UUID
 
 @Component
 @GraphQLApi
@@ -14,4 +15,10 @@ class ProductMutation(private val productService: ProductService) {
 
     @GraphQLMutation
     fun createProduct(input: @GraphQLNonNull ProductInput): @GraphQLNonNull Product = productService.create(input)
+
+    @GraphQLMutation
+    fun updateProduct(id: @GraphQLNonNull UUID, input: @GraphQLNonNull ProductInput): @GraphQLNonNull Product = productService.update(id, input)
+
+    @GraphQLMutation
+    fun deleteProduct(id: @GraphQLNonNull UUID) = productService.deleteById(id)
 }
