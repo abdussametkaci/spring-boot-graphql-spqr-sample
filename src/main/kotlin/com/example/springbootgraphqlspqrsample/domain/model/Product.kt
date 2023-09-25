c5d243cf274a6c0a1ba77cbf74bfe61fdf71e494
Abdussamet KACI <abdussamet.kaci@d-teknoloji.com.tr>
2023-09-25 11:46:08 +0300

    Add @GraphQLNonNull for Product#id - normally id cannot be null - but in repository layer, if id is null, create it, otherwise update it

--- src/main/kotlin/com/example/springbootgraphqlspqrsample/domain/model/Product.kt
+++ src/main/kotlin/com/example/springbootgraphqlspqrsample/domain/model/Product.kt
@@ -1,10 +1,11 @@
 package com.example.springbootgraphqlspqrsample.domain.model
 
+import io.leangen.graphql.annotations.GraphQLNonNull
 import java.math.BigDecimal
 import java.util.UUID
 
 data class Product(
-    var id: UUID? = null,
+    var id: @GraphQLNonNull UUID? = null,
     var name: String,
     var price: BigDecimal,
     var count: Int
